d347191308562d2479f21cf38d43d1cda99cf64e
arijus <arijuss@wix.com>
2014-11-06 17:17:40 +0200

    what did we do?

--- specs/Game.spec.js
+++ specs/Game.spec.js
@@ -11,12 +11,16 @@ describe("GOL", function() {
     expect(game instanceof Game).toBeTruthy();
   });
 
-  it('should have matrix generator', function () {
-    expect(typeof game.generateMatrix).toBe('function');
+  it('should have state initalizer', function () {
+    expect(typeof game.createState).toBe('function');
   });
 
-  it('should have matrix getter', function() {
-    expect(typeof game.getMatrix).toBe('function');
+  it('should have state getter', function() {
+    expect(typeof game.getState).toBe('function');
+  });
+
+  it('should have state setter', function() {
+    expect(typeof game.getState).toBe('function');
   });
 
   it('should have render', function() {
@@ -35,11 +39,36 @@ describe("GOL", function() {
     expect(typeof game.tick).toBe('function');
   });
 
+  it('ticker should work', function () {
+    
+  });
+
+  it('should have next state creator', function() {
+    expect(typeof game.createNextState).toBe('function');
+  });
+
   it('should have runner', function() {
     expect(typeof game.run).toBe('function');
   });
 
-  it('should run without exception', function() {
-    game.run();
+  it('createState should work', function () {
+    expect(function() {
+      game.createState(100, 100, 10);
+    }).not.toThrow();
+  });
+
+  it('createState should return a valid state', function () {
+    var height = 100;
+    var width = 100;
+    var state = game.createState(width, height, 10);
+    expect(Array.isArray(state)).toBe(true);
+    expect(state.length).toBe(height);
+    state.forEach(function(line) {
+      expect(line.length).toBe(width);
+      expect(Array.isArray(line)).toBe(true)
+      line.forEach(function(cell) {
+        expect(typeof cell).toBe('boolean');
+      });
+    });
   });
 });
--- src/Game.js
+++ src/Game.js
@@ -1,16 +1,26 @@
 'use strict';
 
 function Game() {
-  var matrix = [];
-
+  
 }
 
-Game.prototype.generateMatrix = function() {
-  
+Game.prototype._state = null;
+
+Game.prototype.createState = function(width, height, cellSize) {
+  var state = [];
+
+  for(var line = 0; line < height; line++) {
+    state.push([]);
+    for(var col = 0; col < width; col++) {
+      state[line].push(false);
+    }
+  }
+
+  return state;
 };
 
-Game.prototype.getMatrix = function() {
-  
+Game.prototype.getState = function() {
+  return this._state;
 };
 
 Game.prototype.render = function() {
@@ -22,13 +32,35 @@ Game.prototype.getAliveNeighbourCount = function() {
 };
 
 Game.prototype.createCanvas = function() {
-  
+  var canvas = document.createElement('canvas');
+  canvas.width = 100;
+  canvas.height = 100;
+  return canvas;
+};
+
+Game.prototype.createNextState = function() {
+  // TODO write test first (copy of createState test)
+  //var nextState = this.createState();
+  //return nextState;
 };
 
 Game.prototype.tick = function() {
-  
+  // TODO write test first
+  //this.setState(this.createNextState());
 };
 
 Game.prototype.run = function() {
-  
+  this.initializeState();
+  var canvas = this.createCanvas();
+
+  document.body.appendChild(canvas);
+
+  var nextFrame = function() {
+    window.requestAnimationFrame(function() {
+      this.tick();
+      this.render(canvas, this.getState());
+      nextFrame.call(this);
+    }.bind(this));
+  };
+  nextFrame.call(this);
 };
